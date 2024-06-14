ab5d1a3c8c75ba155ec3e86f66c636bbef68dfe0
bhargavipilli <144048169+bhargavipilli@users.noreply.github.com>
2024-06-14 17:16:05 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Hello GeeksForGeeks !!!")
+st.checkbox("Show/Hide")
+st.selectbox("Hobbies: ", ['Dancing', 'Reading', 'Sports'])
+st.button("Click me for no reason")
